eaa76fd3c8097e433faec125a90a82bb90fe67ef
unknown <enriquez.jliquigan@gmail.com>
2022-02-01 12:05:57 +0800

    update

--- weather.py
+++ weather.py
@@ -1,7 +1,8 @@
 # importing requests and json
 import requests, json
+import datetime as dt
 # base URL
-#BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+#BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?"
 BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?"
  #City Name CITY = "Porto"
  #API key API_KEY = "d924d035da69ff52f3677787eee9457b"
@@ -9,7 +10,8 @@ openWeatherMapApiKey = "1ae76ac0b8679d9b65ae01f37ea44b16"
 id = "1683340"
 # upadting the URL
 #URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
-URL = BASE_URL + "id=" + id + "&appid=" + openWeatherMapApiKey
+#URL = BASE_URL + "id=" + id + "&appid=" + openWeatherMapApiKey
+URL = "http://api.openweathermap.org/data/2.5/onecall?lat=14.074368&lon=121.147179&exclude=minutely&appid=1ae76ac0b8679d9b65ae01f37ea44b16"
 #URL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1ae76ac0b8679d9b65ae01f37ea44b16"
 # HTTP request
 response = requests.get(URL)
@@ -17,29 +19,20 @@ response = requests.get(URL)
 mylist =[]
 mylist.append(
 	'Temperature\t'
-	+'Humidity\t'
 	+'Weather\t'
-	+'Weather Description\t'
-	+'Wind Speed\t'
-	+'Wind Degree\t'
 	+'Date')
 if response.status_code == 200:
 	data = response.json()
-	for det in data['list']:
-		print('tem:',det['main']['temp'],";"
-		+'humidity:',det['main']['humidity'],";"
-		+'weatherMain:',det['weather'][0]['main'],";"
-		+'weatherDescription:',det['weather'][0]['description'],";"
-		+'windSpeed:',det['wind']['speed'],";"
-		+'windDegree:',det['wind']['deg'],";"
-		+'date:',det['dt_txt'])
-		mylist.append('{}\t'.format(det['main']['temp'])
-		+'{}\t'.format(det['main']['humidity'])
-		+'{}\t'.format(det['weather'][0]['main'])
+	for det in data['hourly']:
+		print('Temp:',det['temp'],';',
+                'Weather:',det['weather'][0]['description'],';',
+                'Date:',dt.datetime.fromtimestamp(det['dt']).strftime('%Y/%m/%d %H:%M:%S')
+                )
+                
+		mylist.append('{}\t'.format(det['temp'])
 		+'{}\t'.format(det['weather'][0]['description'])
-		+'{}\t'.format(det['wind']['speed'])
-		+'{}\t'.format(det['wind']['deg'])
-		+'{}'.format(det['dt_txt']))
+		+'{}'.format(dt.datetime.fromtimestamp(det['dt']).strftime('%Y/%m/%d %H:%M:%S')))
+		
 		
 else:
    # showing the error message
